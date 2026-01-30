3bbf4002443ad10b4026f2db504b0868c41350d6
Emanuel Ignat <EIgnat@boylesports.com>
2026-01-30 18:02:31 +0200

    angular v2

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,12 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('documentvault-client');
+  inputFile: File | null = null;
+
+  onFileUploaded(files: FileList | null) {
+    if (files == null || files.length == 0) return;
+
+    this.inputFile = files.item(0);
+    debugger;
+  }
 }
